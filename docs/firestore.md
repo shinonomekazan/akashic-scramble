02495cc63c1156038124a8bc23794442f8dd6347
tsugehara <tsugehara@gmail.com>
2026-01-29 11:40:24 +0900

    PlaceとPlayの設計

---
+++ docs/firestore.md
@@ -0,0 +1,109 @@
+# Firestoreのデータ構造
+
+## 前提文書
+
+[プレイヤーの状態遷移](https://github.com/shinonomekazan/akashic-docs/blob/main/planning/player-state.md)を参照。
+
+## Type Define
+
+[TypeScriptコード](../functions/src/types/firestore.ts)を参照。
+
+## Structure
+
+```
+users/
+	{userId}
+
+places/
+	{placeId}
+
+holdPlaces/
+	{holdPlaceId}
+
+plays/
+	{playId}
+		playTokens/
+			{playTokenId}
+
+```
+
+1. Placeはほぼマスターデータとして機能する
+	- 唯一、currentHoldPlayIdのみが更新される
+2. PlaceからHoldPlaceが生成され、一つの確保された枠を表現する
+3. HoldPlaceからPlayが生成され、一つのプレイを表現する
+4. PlayからPlayTokenが生成され、ユーザーのプレイ権を表現する
+
+子コレクションは現状ほぼ利用していない。ほぼ、単一フィールドインデックスで用途を網羅できそうなため。以下に代表的な検索方法を例示する。
+
+1. 自分のHold履歴を参照する: HoldPlaceのownerUserIdで検索
+2. 自分のPlay履歴を参照する: PlaceのownerUserIdで検索
+3. そのPlaceのHoldPlaceの履歴を参照する: HoldPlaceのplaceIdで検索
+4. そのHoldPlaceのPlay履歴を参照する: PlayのholdPlaceIdで検索
+
+複合インデックスが必要なケースは、以下のようなもの。
+
+1. そのコンテンツが現在遊ばれているHoldPlaceを検索する: PlayのproviderIdとcontentCodeとstateで検索
+
+## 利用シーン別の使い方
+
+### そのPlaceの状態を確認する
+
+PlaceのstateまたはcurrentHoldPlaceIdを参照する。
+
+### そのPlaceを確保（Hold）する
+
+1. Placeのbehavioursに応じてHoldPlaceを生成
+2. Placeのstateをholdingに変更し、PlaceのcurrentHoldPlaceIdに値を入れる
+
+### Playを開始する
+
+1. HoldPlaceのbehavioursに応じてPlayを生成
+2. HoldPlaceのcurrentPlayIdに値を入れる
+
+### Playに参加する
+
+1. ユーザーのログイン状態と、PlayのownerUserId、PlayのdefaultPermissionを元にPlayTokenを作成
+
+### Playを開始する
+
+1. PlayのjoinedPlayerIdsを更新する
+2. Play Eventとしてjoinを発行
+
+後述するPlayの状態変化の監視で、joinedPlayerIdsの変化を監視しておけば、現在のJoinedPlayerの変化を検知することも可能。
+
+### Playを停止する
+
+1. PlayのjoinedPlayerIdsを更新する
+2. Play Eventとしてleaveを発行
+
+後述するPlayの状態変化の監視で、joinedPlayerIdsの変化を監視しておけば、現在のJoinedPlayerの変化を検知することも可能。
+
+### Placeの状態変化を監視する
+
+1. Placeをwatchする
+2. PlaceのcurrentHoldPlaceIdの値の変化で検知する
+
+### HoldPlaceの状態変化を監視する
+
+1. HoldPlaceをwatchする
+2. HoldPlaceのcurrentPlayIdの値の変化で、プレー状態を検知する
+3. HoldPlaceのendedAtの値の変化で、HoldPlaceの終了を検知する
+
+HoldPlaceであれば、Placeのwatchをしないようにすること。
+
+### Playの状態変化を監視する
+
+1. Playをwatchする
+2. Playのstateを見てPlayの終了を検知する
+
+Playの監視中にHoldPlaceは監視することはありえるが、PlayのstateはHoldPlaceの状態変更で変化する事を保証し、極力二重監視はしないで済むよう実装する。
+
+### タイルでのPlace状態
+
+タイルUIでPlaceの状態を一括監視するには、すべてのPlaceをwatchしなければならないので非常に効率が悪い。
+
+当初タイルUIでのリアルタイム状態変化は行わないものとするが、将来的にはTileを作り、そのTileに含まれるPlaceの状態はそのTileの監視のみでできるようにしたい。
+
+Tileに含まれるPlaceの情報は、Tileドキュメント単体に全て記載され、単一ドキュメントのwatchで複数Placeの状態を監視できるものとする。TileドキュメントはCloud Firestore triggersによって、やや時間差のある形で反映される形を想定する。
+
+リアルタイム監視を行う事になったら詳細実装に移るものとする。
---
+++ functions/src/types/firestore.ts
@@ -0,0 +1,198 @@
+import { Timestamp } from "firebase-admin/firestore";
+
+// Note: 予約必須とか、ユーザー確保不可とかを後日追加
+export type PlaceBehaviourType = "holdableTime" | "connectionLimit" | "defaultPermission";
+
+export type PlayTokenState = "active" | "used";
+
+/**
+ * そのプレイに対する権利。
+ * - Player: 操作権あり
+ * - Viewer: 閲覧のみ
+ * 権限は細かくはGuest ViewerやJoined Player、Administrator等があるが、ここで定義されるのは2値のみ。
+ */
+export type PlayPermission = "player" | "viewer";
+
+export interface BasePlaceBehaviour {
+	type: PlaceBehaviourType;
+}
+
+/**
+ * 保持可能な時間の制限
+ */
+export interface PlaceHoldableTimeBehaviour extends BasePlaceBehaviour {
+	type: "holdableTime";
+	time: number;
+}
+
+/**
+ * 同時接続数の制限
+ */
+export interface PlaceConnectionLimitBehaviour extends BasePlaceBehaviour {
+	type: "connectionLimit";
+	limit: number;
+}
+
+/**
+ * そのPlaceで作られるPlayのデフォルトパーミッション
+ */
+export interface PlaceDefaultPermissionBehaviour extends BasePlaceBehaviour {
+	type: "defaultPermission";
+	permission: PlayPermission;
+}
+
+export type PlaceBehaviour =
+	| PlaceHoldableTimeBehaviour
+	| PlaceConnectionLimitBehaviour
+	| PlaceDefaultPermissionBehaviour;
+
+export interface Place {
+	x: number;
+
+	y: number;
+
+	name: string;
+
+	behaviours: PlaceBehaviour[];
+
+	/**
+	 * そのPlaceを保持しているHoldPlaceのID。
+	 * HoldPlaceが存在しない場合はundefinedで、undefinedであれば空き地であることが保証される。
+	 */
+	currentHoldPlaceId?: string;
+
+	createdAt: Timestamp;
+
+	updatedAt: Timestamp;
+
+	// Note: currentDefaultPermission等を定義し、Hold Userが上書きできるようにするべき？
+	// Note: 後日予約に関する情報を入れる必要があると思うが、ReservedPlace等別のコレクションにするべきかも？
+}
+
+export interface HoldPlace {
+	placeId: string;
+
+	/**
+	 * そのHoldPlaceのbehaviours。
+	 * 通常Placeのbehavioursと同じ値が入るが、履歴管理の意味で非正規化して保持する。
+	 */
+	behaviours: PlaceBehaviour[];
+
+	/**
+	 * そのHoldPlaceを保有しているユーザーのID。
+	 * そのHoldPlaceを保有しているユーザーがいない場合はundefined。
+	 * この値が無い時は、システムが保有しているという扱いになる。
+	 */
+	holdUserId?: string;
+
+	/**
+	 * 現在そのHoldPlaceでプレーされているPlayのID。
+	 */
+	currentPlayId?: string;
+
+	/**
+	 * HoldPlaceの有効期限。
+	 * Timestamp型だが、分が最小単位であることが保証される。
+	 * この値が存在しない場合、無期限か、時限以外の権利で保有されることを意味する。
+	 */
+	expireAt?: Timestamp;
+
+	/**
+	 * そのHoldPlaceのHoldが終了した日時。
+	 * 手動で終了した場合等、expireAtとは異なる値が入ることがある。
+	 */
+	endedAt?: Timestamp;
+
+	createdAt: Timestamp;
+
+	updatedAt: Timestamp;
+}
+
+export type PlayState = "playing" | "ended";
+
+export interface Play {
+	/**
+	 * そのPlayが実行されているPlaceのID。
+	 */
+	placeId: string;
+
+	/**
+	 * そのPlayが実行されているHoldPlaceのID。
+	 */
+	holdPlaceId: string;
+
+	/**
+	 * コンテンツを提供しているプロバイダーのID。
+	 * 初期はakashic-game-driveのみの想定。
+	 */
+	providerId: string;
+
+	/**
+	 * そのコンテンツをプロバイダー単位で一意に識別するためのコード。
+	 * 初期はakashic-game-driveのcontentId。
+	 */
+	contentCode: string;
+
+	/**
+	 * コンテンツのダウンロードURL。
+	 * 同一のproviderId、同一のcontentCodeでも、バージョン等が違えば異なる事がある。
+	 */
+	contentUrl: string;
+
+	/**
+	 * プレイの状態。
+	 */
+	state: PlayState;
+
+	/**
+	 * そのプレイのオーナーであるユーザーのID。
+	 * 基本的に値があるが、システムがオーナーである場合はundefined。
+	 */
+	ownerUserId?: string;
+
+	/**
+	 * そのプレイに現在参加しているユーザーIDの一覧。
+	 * 履歴として残るものではない点に注意。
+	 */
+	joinedUserIds: string[];
+
+	/**
+	 * そのプレイを見たUserのデフォルトパーミッション。
+	 * プレイが終了している場合や、Guestの場合はこの値に限らずviewerになる。
+	 */
+	defaultPermission: PlayPermission;
+
+	/**
+	 * そのプレイの有効期間。
+	 * Timestamp型だが、分が最小単位であることが保証される。
+	 */
+	expireAt?: Timestamp;
+
+	createdAt: Timestamp;
+
+	updatedAt: Timestamp;
+}
+
+export interface PlayToken {
+	// Note: 親ドキュメントから取得可能なため不要
+	// playId: string;
+	// holdPlaceId: string;
+
+	userId: string;
+
+	token: string;
+
+	permission: PlayPermission;
+
+	serverUrl: string;
+
+	state: PlayTokenState;
+
+	createdAt: Timestamp;
+
+	updatedAt: Timestamp;
+}
+
+export type WithId<T> = T & {
+	id: string;
+};
